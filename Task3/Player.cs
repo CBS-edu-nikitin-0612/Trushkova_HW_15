6d6e955925e36a3928d45ab878c56b2c180b87c7
Juju <kosyuska@yandex.ru>
2022-01-30 04:58:07 +0300

    feat: add Task3
    recording and playing

---
+++ Task3/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Играет");
+        }
+        public void Record()
+        {
+            Console.WriteLine("Записывает");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Пауза");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Закончил");
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task3;
+
+Console.WriteLine("Hello, World!");
+
+Player player = new Player();
+
+IRecodable recording = player as IRecodable;
+recording.Record();
+recording.Pause();
+recording.Record();
+recording.Stop();
+
+IPlayable playering = player as IPlayable;
+playering.Play();
+playering.Pause();
+playering.Stop();
+
+
